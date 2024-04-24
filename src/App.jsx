e12e703c298b9f78780e04fe0b9be7428697dd40
Leonardo Lodi <your_email@address>
2024-04-24 18:28:17 +0100

    do extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,64 @@ import initialEmails from './data/emails'
 
 import './styles/App.css'
 
-function createEmails(email) {
-  let emailClassName = 'email'
+function App() {
+  // Use initialEmails for state
+  console.log(initialEmails)
 
-  if (email.read) {
-    emailClassName += ' read'
-  } else {
-    emailClassName += ' unread'
+  const [emails, setEmails] = useState(initialEmails)
+
+  function toggleRead(id) {
+    setEmails(emails.map(email => {
+      if (email.id === id) {
+        return { ...email, read: !email.read }
+      } else {
+        return email
+      }
+    }))
   }
+
+  function toggleStar(id) {
+    setEmails(emails.map(email => {
+      if (email.id === id) {
+        return { ...email, starred: !email.starred }
+      } else {
+        return email
+      }
+    }))
+  }
+
+  function createEmails(email) {
+    let emailClassName = 'email'
   
-  return (
-    <li key={email.id} className={emailClassName}>
+    if (email.read) {
+      emailClassName += ' read'
+    } else {
+      emailClassName += ' unread'
+    }
+   
+    return (
+      <li key={email.id} className={emailClassName}>
         <div className="select">
           <input
             className="select-checkbox"
-            type="checkbox"/>
+            type="checkbox"
+            checked={email.read}
+            onChange={() => toggleRead(email.id)}
+            />
         </div>
         <div className="star">
           <input
             className="star-checkbox"
             type="checkbox"
             checked={email.starred}
+            onChange={() => toggleStar(email.id)}
           />
         </div>
         <div className="sender">{email.sender}</div>
         <div className="title">{email.title}</div>
-    </li>
-  )
-}
-
-function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
-
-  const [emails, setEmails] = useState(initialEmails)
+      </li>
+    )
+  }
 
   function renderEmails() {
     return emails.map(email => createEmails(email))
