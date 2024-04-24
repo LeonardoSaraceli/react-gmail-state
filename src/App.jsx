8d5f3b358ec9aca9ae835afb65869f6d74446b04
Leonardo Lodi <your_email@address>
2024-04-24 17:10:45 +0100

    do core

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
 
+function createEmails(email) {
+  let emailClassName = 'email'
+
+  if (email.read) {
+    emailClassName += ' read'
+  } else {
+    emailClassName += ' unread'
+  }
+  
+  return (
+    <li key={email.id} className={emailClassName}>
+        <div className="select">
+          <input
+            className="select-checkbox"
+            type="checkbox"/>
+        </div>
+        <div className="star">
+          <input
+            className="star-checkbox"
+            type="checkbox"
+            checked={email.starred}
+          />
+        </div>
+        <div className="sender">{email.sender}</div>
+        <div className="title">{email.title}</div>
+    </li>
+  )
+}
+
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails)
+
+  function renderEmails() {
+    return emails.map(email => createEmails(email))
+  }
 
   return (
     <div className="app">
@@ -38,7 +74,7 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{renderEmails()}</main>
     </div>
   )
 }
